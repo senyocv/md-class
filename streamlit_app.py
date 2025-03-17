21ea8bbced3e65b0a6cb4c16994905be29c53d30
senyocv <kyne.sudiro@gmail.com>
2025-03-17 14:17:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ def main():
   st.title('Dermatology Machine Learning')
   st.info('This app uses machine learning')
 
-  erytheman st.slider('Erythema', min_value=0, max_value=3, value 2)
+  erytheman = st.slider('Erythema', min_value=0, max_value=3, value 2)
 
 if __name__ == "__main__":
   main()
